340b11533ab8804a5716cf2cb11efe3a4e776ae8
ydzhao <zyd2117@gmail.com>
2018-12-08 22:36:08 -0500

    vector IO

--- Utils.h
+++ Utils.h
@@ -3,7 +3,28 @@
 namespace Grid {
 namespace QCD {
 
-  
+template<class T>
+void writeVector(const std::vector<T> &vec, const std::string &filename) {
+
+#ifdef GRID_COMMS_MPI3
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(rank == 0) {
+#endif
+
+    std::ofstream outFile(filename, std::ofstream::app);
+    for(const T& x : vec) outFile << x << " ";
+    outFile << "\n";
+    outFile.close();
+
+#ifdef GRID_COMMS_MPI3
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+}
+
+
 void readField(LatticeGaugeField &U, const std::string &filename)
 {
 		FieldMetaData header;
